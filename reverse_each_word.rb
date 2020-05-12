b872f40170db9509fb55b32656c8bd5e2daf6bd2
Jesse Summer-Lavigne <jessesl22@gmail.com>
2020-05-12 01:10:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,20 @@
+#Each Method
+
 def reverse_each_word(string)
   array = string.split(" ")
   reverse_array = []
   array.each do|string|
+    reverse_array << string.reverse
+  end
+  reverse_array.join(" ")
+end
+
+#Collect Method
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  reverse_array = []
+  array.collect do|string|
     reverse_array << string.reverse
   end
   reverse_array.join(" ")
